29087ed51fd6a57ac3efd4beb9211ad738171723
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-08-17 21:37:52 +0300

    Tests for BloodTestsController added

---
+++ MedicalCenter/MedicalCenter.Tests/Controllers/BloodTestsControllerTest.cs
@@ -0,0 +1,105 @@
+﻿using MedicalCenter.Controllers;
+using MyTested.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MedicalCenter.Tests.Controllers
+{
+    public class BloodTestsControllerTest
+    {
+        [Fact]
+        public void GetViewBloodTestsShouldReturnViewOnlyForDocs()
+            => MyController<BloodTestsController>
+                .Instance()
+                .Calling(c => c.ViewBloodTests("ddd"))
+                .ShouldHave()
+                .ActionAttributes(a => a.RestrictingForAuthorizedRequests("Doctor"))
+                .AndAlso()
+                .ShouldReturn()
+                .View();
+
+        [Fact]
+        public void GetViewBloodTestsShouldBeMapped()
+           => MyRouting
+               .Configuration()
+               .ShouldMap("/BloodTests/ViewBloodTests/dd")
+               .To<BloodTestsController>(c => c.ViewBloodTests("dd"));
+
+
+        [Fact]
+        public void GetSendBloodTestsShouldReturnViewOnlyForDocs()
+            => MyController<BloodTestsController>
+                .Instance()
+                .Calling(c => c.SendBloodTests("ddd"))
+                .ShouldHave()
+                .ActionAttributes(a => a.RestrictingForAuthorizedRequests("Doctor"))
+                .AndAlso()
+                .ShouldReturn()
+                .View();
+
+        [Fact]
+        public void GetSendBloodTestsShouldBeMapped()
+          => MyRouting
+              .Configuration()
+              .ShouldMap("/BloodTests/SendBloodTests/dd")
+              .To<BloodTestsController>(c => c.SendBloodTests("dd"));
+
+        [Fact]
+        public void GetAllBloodTestsShouldReturnViewOnlyForDocs()
+           => MyController<BloodTestsController>
+               .Instance()
+               .Calling(c => c.AllBloodTests("ddd"))
+               .ShouldHave()
+               .ActionAttributes(a => a.RestrictingForAuthorizedRequests())
+               .AndAlso()
+               .ShouldReturn()
+               .View();
+
+        [Fact]
+        public void GetAllBloodTestsShouldBeMapped()
+          => MyRouting
+              .Configuration()
+              .ShouldMap("/BloodTests/AllBloodTests/dd")
+              .To<BloodTestsController>(c => c.AllBloodTests("dd"));
+
+        [Fact]
+        public void GetAllParameterssShouldReturnViewOnlyForDocs()
+           => MyController<BloodTestsController>
+               .Instance()
+               .Calling(c => c.AllParameters("ddd"))
+               .ShouldHave()
+               .ActionAttributes(a => a.RestrictingForAuthorizedRequests())
+               .AndAlso()
+               .ShouldReturn()
+               .View();
+
+        [Fact]
+        public void GetAllParametersShouldBeMapped()
+          => MyRouting
+              .Configuration()
+              .ShouldMap("/BloodTests/AllParameters/dd")
+              .To<BloodTestsController>(c => c.AllParameters("dd"));
+
+        [Fact]
+        public void GetSeeResultsShouldReturnViewOnlyForDocs()
+           => MyController<BloodTestsController>
+               .Instance()
+               .Calling(c => c.SeeResults("ddd"))
+               .ShouldHave()
+               .ActionAttributes(a => a.RestrictingForAuthorizedRequests())
+               .AndAlso()
+               .ShouldReturn()
+               .View();
+
+        [Fact]
+        public void GetSeeResultsShouldBeMapped()
+          => MyRouting
+              .Configuration()
+              .ShouldMap("/BloodTests/SeeResults/dd")
+              .To<BloodTestsController>(c => c.SeeResults("dd"));
+    }
+}
---
+++ MedicalCenter/MedicalCenter.Tests/Controllers/DoctorsControllerTest.cs
@@ -0,0 +1,147 @@
+﻿using MedicalCenter.Controllers;
+using MedicalCenter.Data.Data.Models;
+using MedicalCenter.Services.ViewModels.Doctors;
+using MedicalCenter.Services.ViewModels.Patients;
+using MyTested.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace MedicalCenter.Tests.Controllers
+{
+    public class DoctorsControllerTest
+    {
+        [Fact]
+        public void GetAddDoctorsShouldReturnViewOnlyForPatients()
+           => MyController<DoctorsController>
+               .Instance()
+               .Calling(c => c.Add())
+               .ShouldHave()
+               .ActionAttributes(a => a.RestrictingForAuthorizedRequests("Doctor"))
+               .AndAlso()
+               .ShouldReturn()
+               .View();
+
+        [Fact]
+        public void GetAddDoctorsShouldBeMapped()
+           => MyRouting
+               .Configuration()
+               .ShouldMap("/Doctors/Add")
+               .To<DoctorsController>(c => c.Add());
+
+        [Fact]
+        public void PostAddDoctorsShouldBeMapped()
+            => MyRouting
+                .Configuration().ShouldMap(request => request
+                                       .WithPath("/Doctors/Add")
+                                       .WithMethod(HttpMethod.Post))
+                .To<DoctorsController>(c => c.Add(With.Any<AddDoctorFormModel>()));
+
+        [Fact]
+        public void GetViewProfileShouldBeMapped()
+          => MyRouting
+              .Configuration()
+              .ShouldMap("/Doctors/ViewProfile")
+              .To<DoctorsController>(c => c.ViewProfile());
+
+        [Fact]
+        public void GetViewProfileShouldReturnViewOnlyForDoctors()
+           => MyController<DoctorsController>
+               .Instance()
+               .Calling(c => c.ViewProfile())
+               .ShouldHave()
+               .ActionAttributes(a => a.RestrictingForAuthorizedRequests())
+               .AndAlso()
+               .ShouldReturn()
+               .View();
+
+        [Fact]
+        public void GetChangeProfileShouldReturnViewOnlyForDoctors()
+           => MyController<DoctorsController>
+               .Instance()
+               .Calling(c => c.ChangeProfile("dddd"))
+               .ShouldHave()
+               .ActionAttributes(a => a.RestrictingForAuthorizedRequests())
+               .AndAlso()
+               .ShouldReturn()
+               .View();
+
+        [Fact]
+        public void GetCHangeProfileShouldBeMapped()
+          => MyRouting
+              .Configuration()
+              .ShouldMap("/Doctors/ChangeProfile/dddd")
+              .To<DoctorsController>(c => c.ChangeProfile("dddd"));
+
+        [Fact]
+        public void GetFindPatientByEGNShouldBeMapped()
+          => MyRouting
+              .Configuration()
+              .ShouldMap("/Doctors/FindPatientByEGN")
+              .To<DoctorsController>(c => c.FindPatientByEGN());
+
+        [Fact]
+        public void GetFindPatientByEGNShouldReturnViewOnlyForDoctors()
+           => MyController<DoctorsController>
+               .Instance()
+               .Calling(c => c.FindPatientByEGN())
+               .ShouldHave()
+               .ActionAttributes(a => a.RestrictingForAuthorizedRequests())
+               .AndAlso()
+               .ShouldReturn()
+               .View();
+
+        [Theory]
+        [InlineData("9211067524")]
+        public void PostPatientProfileShouldBeOnlyForPatientsAndRedirectWithValidModel(string egn)
+            => MyController<DoctorsController>
+                .Instance(controller => controller
+                    .WithUser())
+                .Calling(c => c.PatientProfile(new FindPatientEGNFormModel
+                {
+                    EGN = egn
+
+                }))
+                .ShouldHave()
+                .ActionAttributes(attributes => attributes
+                    .RestrictingForHttpMethod(HttpMethod.Post)
+                    .RestrictingForAuthorizedRequests())
+                .ValidModelState()
+                .AndAlso()
+                .ShouldReturn()
+                .View("FindPatientByEGN");
+
+        [Fact]
+        public void GetAllDocsShouldReturnView()
+           => MyController<DoctorsController>
+               .Instance()
+               .Calling(c => c.AllDoctors())
+               .ShouldReturn()
+               .View();
+
+        [Fact]
+        public void GetAllDocsShouldBeMapped()
+          => MyRouting
+              .Configuration()
+              .ShouldMap("/Doctors/AllDoctors")
+              .To<DoctorsController>(c => c.AllDoctors());
+
+        [Fact]
+        public void GetViewDocShouldBeMapped()
+          => MyRouting
+              .Configuration()
+              .ShouldMap("/Doctors/ViewDoctor/aaaa")
+              .To<DoctorsController>(c => c.ViewDoctor("aaaa"));
+
+        [Fact]
+        public void GetViewDochouldReturnView()
+           => MyController<DoctorsController>
+               .Instance()
+               .Calling(c => c.ViewDoctor("aaaa"))
+               .ShouldReturn()
+               .Redirect();
+    }
+}
+
