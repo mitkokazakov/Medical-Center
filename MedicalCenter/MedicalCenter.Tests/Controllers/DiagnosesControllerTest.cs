9b76fe310a747418751dda284ec26ecaf6497389
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-08-17 21:42:09 +0300

    Tests for Dignosesontroller added

---
+++ MedicalCenter/MedicalCenter.Tests/Controllers/DiagnosesControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using MedicalCenter.Controllers;
+using MyTested.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace MedicalCenter.Tests.Controllers
+{
+    public class DiagnosesControllerTest
+    {
+        [Fact]
+        public void GetWriteDiagnoseShouldReturnViewOnlyForDocs()
+            => MyController<DiagnosesController>
+                .Instance()
+                .Calling(c => c.WriteDiagnose("dd"))
+                .ShouldHave()
+                .ActionAttributes(a => a.RestrictingForAuthorizedRequests("Doctor"))
+                .AndAlso()
+                .ShouldReturn()
+                .View();
+
+        [Fact]
+        public void GetWriteDiagnoseShouldBeMapped()
+           => MyRouting
+               .Configuration()
+               .ShouldMap("/Diagnoses/WriteDiagnose/dd")
+               .To<DiagnosesController>(c => c.WriteDiagnose("dd"));
+    }
+}
